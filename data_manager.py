2e73d3cca9963f43e9d846c112a9b23ebd4b7bc7
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-04-01 14:38:03 +0200

    list question route added

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,12 @@
 import connection
+
+
+def get_answers():
+    data = connection.get_data_from_file(connection.ANSWER_FILE_PATH)
+
+    return data
+
+def get_questions():
+    data = connection.get_data_from_file(connection.QUESTION_FILE_PATH)
+
+    return data
--- server.py
+++ server.py
@@ -1,2 +1,21 @@
 import data_manager
 import util
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route('/list')
+@app.route('/')
+def list_question():
+    data = data_manager.get_questions()
+
+    return render_template('list_questions.html',
+                           data = data,
+                           title = "List questions")
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True
+    )
