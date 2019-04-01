1a932b376926b392cd074580b5bcb81004f554ae
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-04-01 14:00:31 +0200

    file reader and writer implemented

--- connection.py
+++ connection.py
@@ -1,1 +1,39 @@
-import
+import csv
+
+ANSWER_FILE_PATH = "answers.csv"
+QUESTION_FILE_PATH = "questions.csv"
+
+ANSWERS_HEADER = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+
+QUESTIONS_HEADER = [ "id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+
+def get_data_from_file(filename):
+    data = []
+
+    with open(filename) as file:
+
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            data.append(dict(row))
+
+    return data
+
+
+
+dictionary = {"id" : 12}
+
+def write_data_to_file(dictionary, filename, fieldnames):
+    data = get_data_from_file(filename)
+
+    with open(filename, "w") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in data:
+            writer.writerow(row)
+
+        writer.writerow(dictionary)
+
+
+
